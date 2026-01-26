36cfdf092f1635b904bc46f89ba4577c999214d8
Rodrigo Mascarenhas <rodrigo.mascarenhas@foxconn.com>
2026-01-26 13:33:57 -0400

    a

--- vite.config.ts
+++ vite.config.ts
@@ -20,8 +20,8 @@ export default defineConfig(({ mode }) => ({
       registerType: "autoUpdate",
       includeAssets: ["favicon.ico", "robots.txt"],
       manifest: {
-        name: "WebSocket Buddy",
-        short_name: "WS Buddy",
+        name: "WebSocketTest",
+        short_name: "WebTest",
         description: "WebSocket testing and debugging tool",
         theme_color: "#000000",
         background_color: "#ffffff",
